bd8cf07ab957990cfbea0d6abccb9c812bba2eb4
Khairuzzaman Ronny <ronny@bitmascot.com>
2023-09-14 22:10:20 +0600

    First commit

---
+++ src/main/java/com/example/lostfiendchild/LostFiendChildApplication.java
@@ -0,0 +1,13 @@
+package com.example.lostfiendchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostFiendChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostFiendChildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lostfiendchild/LostFiendChieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lostfiendchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFiendChieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
